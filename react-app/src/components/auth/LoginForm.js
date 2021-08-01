6e1c6f37f3fbedb8ab1f935a0417ba195719f851
zachmyu <zach.m.yu@gmail.com>
2021-08-01 12:21:12 -0700

    removed console log from login test

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -13,7 +13,6 @@ const LoginForm = () => {
   const onLogin = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
-    console.log(data)
     if (data) {
       setErrors(data);
     }
