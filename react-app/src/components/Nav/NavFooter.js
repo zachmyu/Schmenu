4c8ecc8f6c2629c9a84acc82b22e41c8f6a80846
zachmyu <zach.m.yu@gmail.com>
2021-08-01 13:51:40 -0700

    Updated main site font and footer styling

---
+++ react-app/src/components/Nav/NavFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css';
+
+function NavFooter() {
+
+    return (
+        <div className="footer-container">
+            <a href="https://www.linkedin.com/in/zach-yu-9b86ab1b/" className="footer-element"><i className="fab fa-linkedin"></i> Linked In</a>
+            <a href="https://github.com/zachmyu" className="footer-element"><i className="fab fa-github"></i> Github</a>
+            <a href="http://zachmyu.github.io/" className="footer-element"><i className="far fa-user-circle"></i> Created by Zach Yu</a>
+        </div>
+    );
+}
+
+export default NavFooter;
