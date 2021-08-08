31904c0c19a786717020f0d741670ab2c68e311d
zachmyu <zach.m.yu@gmail.com>
2021-08-08 01:07:51 -0700

    Completed styling for list of restaurants

--- react-app/src/components/RestaurantPage/RestaurantsList.js
+++ react-app/src/components/RestaurantPage/RestaurantsList.js
@@ -1,8 +1,49 @@
-function RestaurantsList() {
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllRestaurants } from "../../store/restaurant";
+import { useParams } from "react-router-dom";
+import './Restaurant.css'
+
+
+function Restaurant() {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const currUser = useSelector(state => state?.session?.user)
+    const allRests = useSelector(state => state?.restaurants)
+    const restObjs = Object.values(allRests)
+
+    // const itemInfo = Object.values(restaurantItems)
+
+    useEffect(() => {
+        dispatch(getAllRestaurants())
+    }, [dispatch])
+
+
+
     return (
-        <h1>List Of Restaurants Here</h1>
+        <>
+            {restObjs &&
+                <div className='container-restaurants-background'>
+                    <h1>Browse by list of Restaurants</h1>
+                    {restObjs?.map(restaurant => (
+                        <div className='container-restaurant' >
+                            <a href={`/restaurants/${restaurant?.id}`} key={restaurant?.id}>
+                                <div className='container-restaurants-details'>
+                                    <img src={restaurant?.restaurant_pix}
+                                        alt={restaurant?.name}
+                                        className='restaurant-thumbnail' />
+                                    <span>{restaurant?.description}</span>
+                                </div>
+                                <div className='container-restaurant-nametype'>
+                                    <span>Type: {restaurant?.restaurant_type}</span>
+                                    <h2>{restaurant?.name}</h2>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            }
+        </>
     )
-
 }
-
-export default RestaurantsList;
+export default Restaurant
