347ba8f5d8c932c95fb05977d83e9f52028a5dd2
zachmyu <zach.m.yu@gmail.com>
2021-08-27 23:56:50 -0700

    Updated User page to test format for mapping format

--- react-app/src/components/UsersPage/User.js
+++ react-app/src/components/UsersPage/User.js
@@ -1,66 +1,104 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useHistory } from 'react-router-dom';
+import { getAllOnwerRestaurants } from '../../store/restaurant';
+import { getAllUserRatings } from '../../store/rating';
+import { getAllUserSaves } from '../../store/save';
+import './User.css'
 
 function User() {
-  const history = useHistory()
-  const currUser = useSelector(state => state?.session?.user);
-  const [user, setUser] = useState({});
-  const { userId } = useParams();
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const currUser = useSelector(state => state?.session?.user);
 
-  useEffect(() => {
-    if (!userId) {
-      return;
-    }
-    (async () => {
-      const response = await fetch(`/api/users/${userId}`);
-      const user = await response.json();
-      setUser(user);
-    })();
-  }, [userId]);
+	const restaurantList = useSelector(state => state?.restaurants)
+	const ratingList = useSelector(state => state?.ratings)
+	const saveList = useSelector(state => state?.saves)
+	const ownerRestsArr = Object.values(restaurantList)
+	const userRatesArr = Object.values(ratingList)
+	const userSavesArr = Object.values(saveList)
 
-  if (!user) {
-    history.push('/');
-  }
+	const [user, setUser] = useState({});
+	const { userId } = useParams();
 
-  let userPage;
-  if (currUser === userId && currUser.account_type === "Owner") {
-    userPage = (
-      <div className='navbar-element-sessionCont'>
-        <li> Space for owner restaurants</li>
-      </div >
-    );
-  } else if (currUser === userId && currUser.account_type === "Reviewer") {
-    userPage = (
-      <div className='navbar-element-sessionCont'>
-        <li> Space for created reviews</li>
-      </div>
-    );
-  } else {
-    userPage = (
-      <div className='navbar-element-loggedOut'>
-        <li> Update user info</li>
-      </div>
-    );
-  }
+	useEffect(() => {
+		if (!userId) return;
+		(async () => {
+			const response = await fetch(`/api/users/${userId}`);
+			const user = await response.json();
+			setUser(user);
+		})();
+		dispatch(getAllOnwerRestaurants(userId));
+		dispatch(getAllUserRatings(userId));
+		dispatch(getAllUserSaves(userId));
+	}, [dispatch, userId]);
 
-  return (
-    <ul>
-      <li>
-        <strong>User Id</strong> {userId}
-      </li>
-      <li>
-        <strong>Username</strong> {user.username}
-      </li>
-      <li>
-        <strong>Email</strong> {user.email}
-      </li>
-      {userPage}
-      <li> Space for owner restaurants</li>
-      <li> Space for created reviews</li>
-      <li> Update user info</li>
+	if (!user) {
+		return null;
+	}
 
-    </ul>
+	let userPage;
+	if (currUser === userId && currUser.account_type === "Owner") {
+		userPage = (
+			<div className='navbar-element-sessionCont'>
+				<ul>
+					{ownerRestsArr && ownerRestsArr?.map(restaurant => (
+						restaurant?.name
+					))}
+				</ul>
+				<ul>
+					{userSavesArr && userSavesArr?.map(save => (
+						save?.restaurant_id
+					))}
+				</ul>
+				<button> Update user info</button>
+			</div >
+		);
+	} else if (currUser === userId && currUser.account_type === "Reviewer") {
+		userPage = (
+			<div className='navbar-element-sessionCont'>
+				<ul>
+					{ownerRestsuserRatesArrArr && userRatesArr?.map(rating => (
+						rating?.rating
+					))}
+				</ul>
+				<ul>
+					{userSavesArr && userSavesArr?.map(save => (
+						save?.restaurant_id
+					))}
+				</ul>
+				<button> Update user info</button>
+			</div>
+		);
+	} else {
+		userPage = (
+			<div className='navbar-element-loggedOut'>
 
-  );
+				<h1>Please log in as user to update info!</h1>
+
+
+			</div>
+		);
+	}
+
+	return (
+		<ul>
+			<li>
+				<div className='userDetail'>User Id: {userId}</div>
+			</li>
+			<li>
+				<div className='userDetail'>Username: {user.username}</div>
+			</li>
+			<li>
+				<div className='userDetail'>Email: {user.email}</div>
+			</li>
+			{userPage}
+			<li> Space for owner restaurants</li>
+			<li> Space for created reviews</li>
+			<li> Update user info</li>
+
+		</ul>
+
+	);
 }
 export default User;
